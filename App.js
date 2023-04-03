ff47af6f4fec5a4a61f20fc7e6d6268a048a5e4e
sajan siwa <saj.siwa@gamil.com>
2023-04-03 19:46:01 +0545

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Store from "./apps/redux/store";
+import { Provider, useSelector } from "react-redux";
+import { Screen } from "react-native-screens";
+// import Navigator from "./routes/navStack";
+import { NavigationContainer } from "@react-navigation/native";
+import { MainStackNavigator } from "./routes/navStack";
+
 
 export default function App() {
+ 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={Store}>
+      <NavigationContainer>
+        <MainStackNavigator/>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
